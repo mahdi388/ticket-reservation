b082a0f10cafd50e517bfde169348af5f8fb0e3f
mahdi388 <ahangarym34@yahoo.com>
2023-07-15 17:59:21 +0330

    add chair reservation feature

--- concert-ticket/src/App.js
+++ concert-ticket/src/App.js
@@ -1,17 +1,55 @@
-import {useEffect,useReducer} from 'react'
+import {useEffect,useReducer,createContext,useState} from 'react'
 import './App.css'
 import Chair from './components/Chair'
 
-const reducer=(state,{type,data})=>{
-  switch(type){
-    case 0:
-      return data
-    default:
-      return state
-  }
-}
+export const dispatchContext=createContext()
+export const hoverContext=createContext()
 
 function App() {
+  const [hover,setHover]=useState(false)
+  const [count,setCount]=useState(0)
+  const [sum,setSum]=useState(0)
+  const reducer=(state,{type,data})=>{
+    switch(type){
+      case 0:
+        return data
+      case 1:
+        let temp=[]
+        for (let chair of JSON.parse(state)) {
+          if(chair.number==data){
+            switch(chair.state){
+              case "unselected":
+                chair.state="selected"
+                break
+              case "selected":
+                chair.state="ready"
+                setTimeout(() => {
+                  dispatch({type:2,data:chair.number})
+                }, 10000);
+                break
+              case "ready":
+                chair.state="reserve"
+                setCount(count+1)
+                setSum(sum+chair.price)
+            }
+          }
+          temp.push(chair)
+        }
+        setHover(true)
+        return JSON.stringify(temp)
+      case 2:
+        let chairs=[]
+        for (let chair of JSON.parse(state)) {
+          if(chair.number==data && chair.state=="ready"){
+            chair.state="unselected"
+          }
+          chairs.push(chair)
+        }
+        return JSON.stringify(chairs)
+      default:
+        return state
+    }
+  }
   const [state,dispatch]=useReducer(reducer,'[]')
   
   useEffect(()=>{
@@ -24,36 +62,49 @@ function App() {
     <div className="stage">
       stage
     </div>
-    <div className="front-chairs">
-      <div className="b">
-        {JSON.parse(state).map(i=>{
-          if(i.section=='B'){
-            return <Chair chair={i}/>
-          }
-        })}
+    <div className='count-sum'>
+      <div>
+        count : {count}
       </div>
-      <div className="a">
-        {JSON.parse(state).map(i=>{
-          if(i.section=='A'){
-            return <Chair chair={i}/>
-          }
-        })}
-      </div>
-      <div className="c">
-        {JSON.parse(state).map(i=>{
-          if(i.section=='C'){
-            return <Chair chair={i}/>
-          }
-        })}
+
+      <div>
+        sum : {sum}
       </div>
     </div>
-    <div className="back-chairs">
-      {JSON.parse(state).map(i=>{
-        if(i.section=='D'){
-          return <Chair chair={i}/>
-        }
-      })}
-    </div>
+    <dispatchContext.Provider value={dispatch}>
+      <hoverContext.Provider value={[hover,setHover]}>
+        <div className="front-chairs" onMouseLeave={()=>setHover(false)}>
+          <div className="b">
+            {JSON.parse(state).map(i=>{
+              if(i.section=='B'){
+                return <Chair chair={i} key={i.bumber}/>
+              }
+            })}
+          </div>
+          <div className="a">
+            {JSON.parse(state).map(i=>{
+              if(i.section=='A'){
+                return <Chair chair={i} key={i.bumber}/>
+              }
+            })}
+          </div>
+          <div className="c">
+            {JSON.parse(state).map(i=>{
+              if(i.section=='C'){
+                return <Chair chair={i} key={i.bumber}/>
+              }
+            })}
+          </div>
+        </div>
+        <div className="back-chairs" onMouseLeave={()=>setHover(false)}>
+          {JSON.parse(state).map(i=>{
+            if(i.section=='D'){
+              return <Chair chair={i} key={i.bumber}/>
+            }
+          })}
+        </div>
+      </hoverContext.Provider>
+    </dispatchContext.Provider>
   </>;
 }
 
--- concert-ticket/src/components/Chair.js
+++ concert-ticket/src/components/Chair.js
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import { dispatchContext,hoverContext } from "../App";
+
 function Chair({chair}) {
-    return <div className="chair" title={chair.price}>{chair.number}</div> 
+    const dispatch=useContext(dispatchContext)
+    const [hover,setHover]=useContext(hoverContext)
+    // console.log(useContext(hoverContext))
+    return <div className={`chair ${chair.state}`}
+                title={chair.price} 
+                onMouseDown={()=>dispatch({type:1,data:chair.number})}
+                onMouseOver={()=>{
+                    if(hover){
+                        dispatch({type:1,data:chair.number})
+                    }
+                }}
+                onMouseUp={()=>setHover(false)}>
+                    {chair.number}
+                </div> 
 }
 
 export default Chair;
